5adefe68e8641d14f7304d73addd2fd9aee5cd1c
Matrix <matrix0415@gmail.com>
2015-01-12 16:49:24 +0800

    comment finish.

--- lemma_example.py
+++ lemma_example.py
@@ -3,10 +3,11 @@ __author__ = 'Matrix'
 # 2015/01/12
 # Script passed in py2 & py3 with Ubuntu 14.04 env.
 # Prerequirement: pip install nltk lingid(for py2)
+# The program will detect language first, then lemma the doc.
 
 from libs.nlpLib import nltkL
 
-nltk =nltkL("libs/nltk_data/")       # folder "nltk_data" location, empty to use the default path
+nltk =nltkL("libs/nltk_data/")       # folder "nltk_data" location, empty to use the default path.
 
 doc ="We are named as the best company in the world. Employees in our company have a great benefit for their future."
 
