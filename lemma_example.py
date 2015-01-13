d0d400e023e4209d1bee68811a10ae8d7fa7eeff
Matrix <matrix0415@gmail.com>
2015-01-14 00:01:45 +0800

    add other language example.

--- lemma_example.py
+++ lemma_example.py
@@ -9,15 +9,27 @@ from libs.nlpLib import nltkL
 
 nltk =nltkL("libs/nltk_data/")       # folder "nltk_data" location, empty to use the default path.
 
-doc ="We are named as the best company in the world. Employees in our company have a great benefit for their future."
+docEng ="We are named as the best company in the world. Employees in our company have a great benefit for their future."
+docChe ="演講題目：哲學經典之應用─以哲學諮商為例"
 
-result =nltk.englishCorpusLemma(doc)
+resultEng =nltk.englishCorpusLemma(docEng)
+resultChe =nltk.englishCorpusLemma(docChe)
 
-print(result)
+print("English corpus ----------")
+print(resultEng)
 
+print("Chinese corpus ----------")
+print(resultChe)
 
 '''
+If your corpus is in English, it will return True in the first, else language are in False.
+It will auto remove the punctuations.
+
 Console Print:::
 
-[True, 'we be name as the best company in the world employees in our company have a great benefit for their future']
+English corpus ----------
+[True, 'we be name as the best company in the world employee in our company have a great benefit for their future']
+Chinese corpus ----------
+[False, '']
+
 '''
