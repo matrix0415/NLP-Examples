30e10bedc61aeb60e50c5b870d15c3246b387dbb
Matrix <matrix0415@gmail.com>
2015-03-10 23:47:14 +0800

    random pick row higher than threshold.

--- fileRead_example.py
+++ fileRead_example.py
@@ -1,31 +1,43 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Matrix'
-
+import re
+import numpy as np
 from libs.fileLib import fileReadLine as readline
 from libs.fileLib import fileWriteLine as writeline
 
-path ="sample_corpus/hotels.com.opinions"
-target ="sample_corpus/test"
+rs =[]
+dataLength =10
+threshold =2
+exceptColumn =[0, 161]
+
+path ="sample_corpus/data.csv"
+target ="sample_corpus/test.csv"
+
+
 # 讀第一至第十行
 # r =readline(path, line=(1,10))     # return List[True/False, List/Error message]
 
 # 讀一千筆
 # r =readline(path, count=1000) # return List[(same as above)]
 
-# line 1
-r1 =readline(path, line=(1,2))     # return List[True/False, List/Error message]
 
-# 隨機一千筆
-r2 =readline(path, random =10)
+r =readline(path, random =dataLength)
 
-if r1[0] and r2:
-	r =r1[1]+r2[1]
+if r[0]:
+	r =[i.split(',') for i in r[1]]
+	tmp =[[i.pop(e) for e in exceptColumn] for i in r]
+	r =[[float(k) for k in i] for i in r]
+	belowThreshold =sorted([key for key, i in enumerate(r) if sum(i)<threshold], reverse=True)
+	[r.pop(i) for i in belowThreshold]
+	[tmp.pop(i) for i in belowThreshold]
+	rs =[tmp[i]+r[i] for i in range(len(r))]
+	rs =[",".join([re.sub("\n|\r","",str(k)) for k in i]) for i in rs]
+	w =writeline(target, rs)
+	print(w)
 
-	# 寫List Obj，用換行代表不同位置的list
-	w =writeline(target, r)
 
-	# 寫List Obj，自定義不同位置list的連接方式為";"
-	# w =writeline(path, k[1], listJoin =";")
 
-else:
-	print(r1[1]+r2[1])
+# 寫List Obj，用換行代表不同位置的list
+# w =writeline(target, rs)
+# 寫List Obj，自定義不同位置list的連接方式為";"
+## w =writeline(path, k[1], listJoin =";")
